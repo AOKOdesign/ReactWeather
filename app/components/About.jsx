ef1166537e721a928e370ac3c1fc06eb8852c505
Adam <adam@aoko.ca>
2016-10-30 18:36:13 -0400

    Style the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,17 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+    <div className="container">
+      <h1 className="text-center">About</h1>
+      <div className="callout primary">
+        <h5>Whoa, you've found the about page!</h5>
+        <p>This is a simple ReactJS weather fetching application utilizing the OpenWeatherMap API.</p>
+        <a href="https://github.com/AOKOdesign/ReactWeather">GitHub Repo</a>
+        <br />
+        <a href="http://openweathermap.org/">OpenWeatherMap</a>
+        <p>Built with love by <a href="http://www.aoko.ca/">AOKO</a></p>
+      </div>
     </div>
   )
 };
